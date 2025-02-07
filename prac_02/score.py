5deb34d2b1d99dec0affb58a6b791046636af8d5
jakekato <loveillusion02@gmail.com>
2025-02-07 18:25:57 +0800

    Refactor grade determine program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,15 +3,23 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    grade = "Invalid score"
-elif score >= 90:
-    grade = "Excellent"
-elif score >= 50:
-    grade = "Passable"
-else:
-    grade = "Bad"
-print(grade)
 
+def main():
+    """get user score and print the determined grade"""
+    score = float(input("Enter score: "))
+    grade = determine_grade(score)
+    print("Your grade is:", grade)
+
+
+def determine_grade(score):
+    """determine grade from passed in score"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    return "Bad"
+
+
+main()
