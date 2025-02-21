e1889a96dbb605a5dcdd1b18fd5dfaa573588173
jakekato <loveillusion02@gmail.com>
2025-02-21 11:19:00 +0800

    Add username access mangement to list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,9 @@
 NUMBER_OF_PROMPTS = 5
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
 
 def main():
-    """Let user input NUMBER_OF_PROMPTS numbers and display information about them"""
+    """Let user input NUMBER_OF_PROMPTS numbers and display information about them and check if username has access"""
     numbers = []
     for i in range(NUMBER_OF_PROMPTS):
         number = int(input("Number: "))
@@ -12,6 +13,12 @@ def main():
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+    username = input("Username: ")
+    if username in USERNAMES:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 main()
