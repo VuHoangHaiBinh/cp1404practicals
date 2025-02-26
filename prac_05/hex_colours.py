3ae139e0ab638018bc32af7b700de30476a07d1e
jakekato <loveillusion02@gmail.com>
2025-02-27 06:01:33 +0800

    Fixed name convention for hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,7 @@
 COLOUR_TO_HEX = {"amaranth": "#e52b50", "antiquewhite": "#faebd7", "apricot": "#fbceb1", "aqua": "#00ffff",
                  "beaver": "#9f8170", "beige": "#f5f5dc", "bistre": "#3d2b1f", "bittersweet": "#fe6f5e",
                  "blueviolet": "#8a2be2", "brass": "#b5a642"}
-maximum_colour_length = max(len(code) for code in COLOUR_TO_HEX.keys())
+maximum_colour_length = max(len(colour_name) for colour_name in COLOUR_TO_HEX.keys())
 
 for colour_name, hex_code in COLOUR_TO_HEX.items():
     print(f"{colour_name:<{maximum_colour_length}} is {hex_code}")
