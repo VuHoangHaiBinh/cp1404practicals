08e6387539aa70704cc69c7f5156bb0c4bd3e631
jakekato <loveillusion02@gmail.com>
2025-03-18 20:00:57 +0800

    Add pointer arithmetic to class construtor

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,12 +7,13 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
@@ -25,10 +26,10 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    rust = ProgrammingLanguage("Rust", "Static", True, 2013)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, False)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, False)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, False)
+    rust = ProgrammingLanguage("Rust", "Static", True, 2013, False)
 
     languages = [ruby, python, visual_basic, rust]
     print(python)
