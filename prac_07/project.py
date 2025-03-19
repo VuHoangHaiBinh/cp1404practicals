27ae19c0e0db277383338d00785285d189a522e5
jakekato <loveillusion02@gmail.com>
2025-03-19 12:32:07 +0800

    Remove end date parameter from Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -8,19 +8,18 @@ DATE_FORMAT = "%d/%m/%Y"
 
 
 class Project:
-    def __init__(self, name: str, start_time: str, end_time: str, priority: int, estimate_budget: float,
+    def __init__(self, name: str, start_date: str, priority: int, estimate_budget: float,
                  completion: float):
         """Initialize class constructor with parameters."""
         self.name = name
-        self.start_time = start_time
-        self.end_time = end_time
+        self.start_date = start_date
         self.priority = priority
         self.estimate_budget = estimate_budget
         self.completion = completion
 
     def __str__(self) -> str:
         """Get formatted class information."""
-        return f"{self.name}, start: {self.start_time}, end: {self.end_time}, priority {self.priority}, estimate: ${self.estimate_budget:.2f}, completion: {self.completion}%"
+        return f"{self.name}, start: {self.start_date},  priority {self.priority}, estimate: ${self.estimate_budget:.2f}, completion: {self.completion}%"
 
     def __lt__(self, other) -> bool:
         """Get class comparison feature."""
@@ -28,15 +27,15 @@ class Project:
 
     def is_after_date(self, date: str) -> bool:
         """Get if class start_time is after the passed in time."""
-        return datetime.strptime(self.start_time, DATE_FORMAT) > datetime.strptime(date, DATE_FORMAT)
+        return datetime.strptime(self.start_date, DATE_FORMAT) > datetime.strptime(date, DATE_FORMAT)
 
 
 def test():
-    project1 = Project("test", "11/11/2020", "11/11/2020", 2, 20.0, 0)
-    print(project1)  # Expect: test, start: 11/11/2020, end: 11/11/2020, priority 2, estimated: $20.00, completion: 0%
+    project1 = Project("test", "11/11/2020", 2, 20.0, 0)
+    print(project1)  # Expect: test, start: 11/11/2020, priority 2, estimated: $20.00, completion: 0%
     print(project1.is_after_date("10/11/2020"))  # Expect: True
 
-    project2 = Project("test", "10/11/2020", "11/11/2020", 1, 20.0, 0)
+    project2 = Project("test", "10/11/2020", 1, 20.0, 0)
     projects = [project1, project2]
     projects.sort()  # Expect project 2 first then 1
     for project in projects:
