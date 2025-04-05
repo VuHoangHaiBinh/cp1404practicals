a21bb8e8651b44a8304e288179a181b4bd87ae58
jakekato <loveillusion02@gmail.com>
2025-04-05 20:05:42 +0800

    Modify taxi class to round trip fare

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,8 +6,10 @@ def test():
     silver_service_taxi = SilverServiceTaxi("Hummer", 200, 4)
     assert str(silver_service_taxi) == "Car name=Hummer, fuel=200, odometer=0, 0km on current fare, $4.92/km plus flagfall of $4.50"
 
-    silver_service_taxi.drive(1)
-    assert silver_service_taxi.get_fare() == (4.92 + 4.5)
+    silver_service_taxi = SilverServiceTaxi("Hummer", 200, 2)
+    silver_service_taxi.drive(18)
+    assert silver_service_taxi.get_fare() == 48.80
+
 
 if __name__ == "__main__":
     test()
