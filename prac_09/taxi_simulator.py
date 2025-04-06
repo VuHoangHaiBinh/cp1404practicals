2c40853ffba16a3ffe852a9fbce27add8f15c2de
jakekato <loveillusion02@gmail.com>
2025-04-06 12:00:40 +0800

    Write main structure for program

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,44 @@
+# TODO: The user should be able to choose from a list of available taxis,
+# They can choose how far they want to drive,
+# TODO: At the end of each trip, show them the trip cost and add it to their bill.
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+def main():
+    current_taxi = None
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    bill = 0
+
+    print("Let's drive!")
+    print(MENU)
+
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            display_taxis(taxis)
+            current_taxi = choose_taxi("Choose taxi: ", taxis)
+
+        elif choice == "d":
+            if current_taxi:
+                price = drive_taxi(current_taxi)
+                print(f"Your {current_taxi.name} cost you ${price:.2f}")
+                bill += price
+            else:
+                print("You need to choose a taxi before you can drive")
+
+        else:
+            print("Invalid option")
+
+        print(f"Bill to date: ${bill:.2f}")
+        choice = input(">>> ").lower()
+
+    print(f"Total trip cost: ${bill:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+
+if __name__ == "__main__":
+    main()
