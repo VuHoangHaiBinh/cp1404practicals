04a53fa8b79795cddcd864c461a92b4f8ebfbedc
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-04-12 21:33:21 +0800

    Formatting and removing TODOs

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -4,6 +4,7 @@ Testing code using assert and doctest
 """
 
 import doctest
+
 from prac_06.car import Car
 
 
@@ -23,6 +24,7 @@ def is_long_word(word, length=5):
     True
     """
     return len(word) >= length
+
 
 def convert_to_sentence(phrase):
     """
@@ -44,7 +46,6 @@ def convert_to_sentence(phrase):
     return sentence
 
 
-
 def run_tests():
     """Run the tests on the functions."""
     # assert test with no message - used to see if the function works properly
@@ -52,19 +53,11 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     car = Car()
     assert car._odometer == 0, "Car does not set odometer correctly"
-
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
-    # Note that Car's __init__ function sets the fuel in one of two ways:
-    # using the value passed in or the default
-    # You should test both of these
 
     # CASE 1: with parameter
     car = Car(fuel=10)
@@ -77,19 +70,4 @@ def run_tests():
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
-# (PyCharm may see your >>> doctest comments and run doctests anyway.)
 doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
