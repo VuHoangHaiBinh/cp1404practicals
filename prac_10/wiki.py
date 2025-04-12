21da0fe7193e4db9dbe018097d684c3ce6f09e69
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-04-12 22:16:06 +0800

    Complete valid search result found

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -6,6 +6,9 @@ def main():
     while title != "":
         try:
             search_result = wikipedia.page(title, auto_suggest=False)
+            print(search_result.title)
+            print(search_result.content)
+            print(search_result.url)
 
         except wikipedia.exceptions.DisambiguationError as suggestion:
             pass
