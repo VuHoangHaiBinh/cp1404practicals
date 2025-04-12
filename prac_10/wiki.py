cad823f5c354e0fe94f75bc618dd86261f6a16ac
VuHoangHaiBinh <vhoanghaibinh@gmail.com>
2025-04-12 22:17:21 +0800

    Add docstring and comments

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,6 +2,7 @@ import wikipedia
 
 
 def main():
+    """Get title until blank and search it up wiki with suggestions."""
     title = input("Enter page title: ").strip()
     while title != "":
         try:
@@ -10,10 +11,12 @@ def main():
             print(search_result.content)
             print(search_result.url)
 
+        # Print suggestions if search title is too abstract
         except wikipedia.exceptions.DisambiguationError as suggestion:
             print("We need a more specific title. Try one of the following, or a new search:")
             print(suggestion.__dict__["options"])
 
+        # Alert not found id in wiki database
         except wikipedia.exceptions.PageError:
             print(f'Page id "{title}" does not match any pages. Try another id!')
 
